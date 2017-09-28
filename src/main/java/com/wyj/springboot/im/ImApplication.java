f8a67e6a1b145ca5911654477a494477ca33d4b2
wyj <997269684@qq.com>
2017-09-28 09:54:37 +0800

    first commit

---
+++ src/main/java/com/wyj/springboot/im/ImApplication.java
@@ -0,0 +1,12 @@
+package com.wyj.springboot.im;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wyj/springboot/im/ImApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wyj.springboot.im;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
